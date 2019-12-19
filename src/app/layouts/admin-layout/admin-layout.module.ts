74ca3db1243f5522f2c71a9f6994928678a4eab2
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2019-12-20 00:55:55 +0100

    adding adminTablesComponent
    application starting on port 4201

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -12,6 +12,7 @@ import { IconsComponent } from '../../pages/icons/icons.component';
 import { MapsComponent } from '../../pages/maps/maps.component';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
+import { AdminTablesComponent} from '../../pages/admin-tables/admin-tables.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 // import { ToastrModule } from 'ngx-toastr';
 
@@ -29,7 +30,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     UserProfileComponent,
     TablesComponent,
     IconsComponent,
-    MapsComponent
+    MapsComponent,
+    AdminTablesComponent
   ]
 })
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthLoginInfo} from '../../auth/login-info';
+import {AuthService} from '../../auth/auth.service';
+import {TokenStorageService} from '../../auth/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +10,46 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+
+  form: any = {};
+  isLoginFailed = false;
+  isLoggedIn = false;
+  errorMessage = '';
+  roles: string[] = [];
+  private loginInfo: AuthLoginInfo;
+
+  constructor(private authService: AuthService, private tokenStorageService: TokenStorageService) {}
 
   ngOnInit() {
+    if(this.tokenStorageService.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorageService.getAuthorities();
+    }
   }
   ngOnDestroy() {
+  }
+
+  onSubmit(formulare: NgForm) {
+      console.log(formulare);
+    this.loginInfo = new AuthLoginInfo(
+      this.form.username,
+      this.form.password
+    );
+      this.authService.attemptAuth(this.loginInfo).subscribe(
+        data => {
+          this.tokenStorageService.saveUsername(data.username);
+          this.tokenStorageService.saveToken(data.accessToken);
+          this.tokenStorageService.saveAuthorities(data.authorities);
+          this.isLoggedIn = true;
+          this.isLoginFailed = false;
+          this.roles = this.tokenStorageService.getAuthorities();
+        },
+        error => {
+          console.log(error);
+          this.errorMessage = error.error;
+          this.isLoginFailed = true;
+        }
+      );
   }
 
 }
